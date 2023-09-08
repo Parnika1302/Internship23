da5637c3c0efc3d727245638d37a845c3cb946e3
Parnika1302 <103065399+Parnika1302@users.noreply.github.com>
2023-09-08 11:49:47 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-connection_string = f"mongodb+srv://parnika1302:Parnika%401302@pythonmongo.ozetbjv.mongodb.net/?retryWrites=true&w=majority"
+connection_string = f"mongodb+srv://parnika1302:**************@pythonmongo.ozetbjv.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
 
 db=client.todo_db
